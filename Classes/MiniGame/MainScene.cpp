f8f16a7594edd2344dd179ffe2202f8d982d397e
Nguyen Trung Hieu <trunghieu.nguyen@hotmail.com>
2017-08-18 17:23:56 +0700

    Update class main

---
+++ Classes/MiniGame/MainScene.cpp
@@ -0,0 +1,9 @@
+//
+//  MainScene.cpp
+//  ChipGame
+//
+//  Created by Nguyen Trung Hieu on 8/18/17.
+//
+//
+
+#include "MainScene.hpp"
---
+++ Classes/MiniGame/MainScene.hpp
@@ -0,0 +1,14 @@
+//
+//  MainScene.hpp
+//  ChipGame
+//
+//  Created by Nguyen Trung Hieu on 8/18/17.
+//
+//
+
+#ifndef MainScene_hpp
+#define MainScene_hpp
+
+#include <stdio.h>
+
+#endif /* MainScene_hpp */
